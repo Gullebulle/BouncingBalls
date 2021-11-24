917a9bd81f554cdeba389e8fc3ad39f294078667
Gullebulle <gustaf1998@telia.com>
2021-11-24 22:33:10 +0100

    Final?

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// setup canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
-
-// function to generate random number
 
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,13 +10,18 @@ function random(min, max) {
 }
 
 let balls = [];
+//My functions starts here, i declare 3 ints which i am going to use in my functions.
 let ballsQuantity = 0;
 let differentSizes = 0;
+let upOrDown = 1;
 
+//Function for adding the ball. The amount can be found in the while loop at the end of the document.
+//I also had to move the while loop into the loop function since it wouldn't draw the balls otherwise.
 function addBall(){
   ballsQuantity++;
 }
 
+//Function for removing the ball, i use an if statement here so that ballsQuantity doesn't become negative.
 function removeBall(){
   if (balls.length > 0){
     ballsQuantity--;
@@ -31,8 +32,35 @@ function removeBall(){
   }
 }
 
+//This function changes so that the "Ball.prototype.draw" function doesn't draw the full circle which creates weird sizes.
 function weirdSizes(){
     differentSizes = random(0, 12);
+}
+
+//This makes the balls go horizontally
+function travelDirectionHorizont(){
+  upOrDown = 2;
+}
+
+//This makes the balls go vertically
+function travelDirectionVertical(){
+  upOrDown = 3;
+}
+
+/*I create the specialLength int since it wouldn't work with "balls.length" in the for 
+loop because the array with the balls in it was being removed at the same time.
+*/
+let specialLength = 0;
+
+//This function fully resets all earlier functions.
+function fullReset(){
+  differentSizes = 0;
+  upOrDown = 1;
+  specialLength = balls.length;
+  for (let i = 0; i < specialLength; i++) {
+    balls.pop();
+    ballsQuantity--;
+  }
 }
 
 function Ball(x, y, velX, velY, color, size) {
@@ -73,9 +101,20 @@ function Ball(x, y, velX, velY, color, size) {
     if ((this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
-  
-    this.x += this.velX;
-    this.y += this.velY;
+    
+    //Here we declare which direction the ball will be traveling
+    if(upOrDown == 1){
+      this.x += this.velX;
+      this.y += this.velY;
+    }
+    else if(upOrDown == 2){
+      this.x += this.velX;
+      this.y == this.velY;
+    }
+    else if(upOrDown == 3){
+      this.x == this.velX;
+      this.y += this.velY;
+    }
   }
 
   Ball.prototype.collisionDetect = function() {
@@ -106,8 +145,6 @@ function loop() {
   while (balls.length < ballsQuantity) {
     let size = random(12,24);
     let ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
       random(0 + size,width - size),
       random(0 + size,height - size),
       random(-4,1),
